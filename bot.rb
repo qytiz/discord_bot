acba5b04eedea0d674b3407c44712a30c2521d2b
qytiz <magronik@mail.ru>
2022-07-18 13:38:59 +0000

    Adding time counter

--- bot.rb
+++ bot.rb
@@ -2,16 +2,17 @@
 require 'discordrb'
 require 'audioinfo'
 require 'byebug'
+require 'redis'
 
 bot = Discordrb::Commands::CommandBot.new token: ENV["BOT_TOKEN"]
-
-
+redis = Redis.new
 
 bot.message(content: 'turn music on') do |event|
   channel = event.user.voice_channel
   bot.voice_connect(channel)
 
   voice_bot = event.voice
+  redis.del(event.server.id)
   played_songs = [0]
   voice_bot.adjust_offset = 10
   time_until_end = 0
@@ -19,8 +20,35 @@ bot.message(content: 'turn music on') do |event|
   while played_songs.length < file_counter do
     song_id = rand_song_id(played_songs)
     played_songs << song_id
-    #time_until_end = get_song_time(song_id) #ToDo:add automatic counter to show how much time left before end track
+    event.respond 'Now play:'
+    event.respond get_song_name(song_id)
+    redis.set(event.server.id,song_id)
     voice_bot.play_file("./music/#{song_id}.mp3") #ToDo: change logic to dynamicly get mp3 fyles from repository
+  end
+  redis.del(event.server.id)
+end
+
+bot.message(content: 'time') do |event|
+  voice_bot = event.voice
+  time_total = song_time_simplified(redis.get(event.server.id))
+  time = [ voice_bot.stream_time.to_i / 60 % 60, voice_bot.stream_time.to_i % 60].map{|t| t.to_s.rjust(2,'0')}.join(':')
+  if time_total
+    event.respond "#{time}/#{ time_total }" 
+  else 
+    event.respond "No music now"
+  end
+end
+
+bot.message(content: 'time left') do |event|
+  voice_bot = event.voice
+  time_total = get_song_time(redis.get(event.server.id))
+  time = voice_bot.stream_time.to_i
+  time_result = time_total-time
+  time_left = [ time_result / 60 % 60, time_result % 60].map{|t| t.to_s.rjust(2,'0')}.join(':')
+  if time_total
+    event.respond "left: #{time_left}" 
+  else 
+    event.respond "No music now"
   end
 end
 
@@ -46,12 +74,24 @@ def rand_song_id(played_songs)
   song_id
 end
 
-def get_song_time(song_id) # method for calculating time for file work
+def song_time_simplified(song_id) 
+  time_until_end = get_song_time(song_id)
+  [ time_until_end / 60 % 60, time_until_end % 60].map{|t| t.to_s.rjust(2,'0')}.join(':')
+end
+
+def get_song_time(song_id)
   time_until_end = 0
   AudioInfo.open("./music/#{song_id}.mp3") do |info| #ToDo: change logic to dynamicly get mp3 fyles from repository
     time_until_end = info.length 
   end
-  time_until_end
+end
+
+def get_song_name(song_id)
+  song_name = nil
+  AudioInfo.open("./music/#{song_id}.mp3") do |info| 
+  song_name = "#{info.artist} - #{info.title} "
+  end
+  song_name + song_time_simplified(song_id).to_s
 end
 
 def file_counter
